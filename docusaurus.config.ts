84f28c48e9e70f143381009da447ff60c329c1b4
tlipoca9 <tlipoca9@gmail.com>
2024-07-31 21:22:14 +0800

    polish

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tlipoca9/tlipoca9.github.io/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tlipoca9/tlipoca9.github.io/tree/main/blog',
         },
         theme: {
           customCss: './src/css/custom.css',
